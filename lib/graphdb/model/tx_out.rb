dc080f5e67fe5b383e04185ac73acc86ea7ba838
azuchi <azuchi@haw.co.jp>
2016-02-26 18:27:34 +0900

    Implements TxOut hash parse logic

--- lib/graphdb/model/tx_out.rb
+++ lib/graphdb/model/tx_out.rb
@@ -4,13 +4,27 @@ module Graphdb
       include Neo4j::ActiveNode
 
       property :value
-      property :vout, type: Integer
+      property :n, type: Integer
       property :script_pubkey_asm
       property :script_pubkey_hex
       property :output_type
-      property :req_sig, type: Integer
+      property :req_sigs, type: Integer
 
       has_one :out, :tx, type: :tx
+
+      def self.from_hash(hash)
+        tx_out = new
+        tx_out.value = hash['value']
+        tx_out.n = hash['n']
+        if hash['scriptPubKey']
+          tx_out.script_pubkey_asm = hash['scriptPubKey']['asm']
+          tx_out.script_pubkey_hex = hash['scriptPubKey']['hex']
+          tx_out.output_type = hash['scriptPubKey']['type']
+          tx_out.req_sigs = hash['scriptPubKey']['reqSigs']
+        end
+        tx_out
+      end
+
     end
   end
 end
---
+++ spec/graphdb/tx_out_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Graphdb::Model::TxOut do
+
+  it 'create txout node' do
+    hash = {
+        'value' => 0.00350000, 'n' => 0,
+        'scriptPubKey' => {
+          'asm' => 'OP_HASH160 50f8a391e9234d8aadc340f00ab9404b231dd742 OP_EQUAL',
+          'hex' => 'a91450f8a391e9234d8aadc340f00ab9404b231dd74287',
+          'reqSigs' => 1,
+          'type' => 'scripthash',
+          'addresses' => ['2MzdMuNFP3gUpg4dZRFYc5KJhBPTa7mCsnm']
+        }
+    }
+    tx_out = Graphdb::Model::TxOut.from_hash(hash)
+    expect(tx_out.value).to eq(0.00350000)
+    expect(tx_out.n).to eq(0)
+    expect(tx_out.script_pubkey_asm).to eq('OP_HASH160 50f8a391e9234d8aadc340f00ab9404b231dd742 OP_EQUAL')
+    expect(tx_out.script_pubkey_hex).to eq('a91450f8a391e9234d8aadc340f00ab9404b231dd74287')
+    expect(tx_out.req_sigs).to eq(1)
+    expect(tx_out.output_type).to eq('scripthash')
+  end
+
+end
