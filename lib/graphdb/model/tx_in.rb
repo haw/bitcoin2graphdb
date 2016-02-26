c96e2597d3df1d585fc5c555ac304ef4252cd338
azuchi <azuchi@haw.co.jp>
2016-02-26 18:19:13 +0900

    Implements TxIn hash parse logic

--- lib/graphdb/model/tx_in.rb
+++ lib/graphdb/model/tx_in.rb
@@ -11,6 +11,20 @@ module Graphdb
       property :sequence
 
       has_one :out, :tx, type: :tx
+
+      def self.from_hash(hash)
+        tx_in = new
+        tx_in.txid = hash['txid']
+        tx_in.vout = hash['vout']
+        if hash['scriptSig']
+          tx_in.script_sig_asm = hash['scriptSig']['asm']
+          tx_in.script_sig_hex = hash['scriptSig']['hex']
+        end
+        tx_in.coinbase = hash['coinbase']
+        tx_in.sequence = hash['sequence']
+        tx_in
+      end
+
     end
   end
 end
