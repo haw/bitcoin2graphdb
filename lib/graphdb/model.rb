bd4e475ba374fb4b54673f86fe856ede097c354b
azuchi <azuchi@haw.co.jp>
2016-03-03 15:37:31 +0900

    Remove database load

--- lib/graphdb/model.rb
+++ lib/graphdb/model.rb
@@ -1,6 +1,5 @@
 module Graphdb
   module Model
-    autoload :Database, 'graphdb/model/database'
     autoload :ActiveNodeBase, 'graphdb/model/active_node_base'
     autoload :Block, 'graphdb/model/block'
     autoload :Transaction, 'graphdb/model/transaction'
