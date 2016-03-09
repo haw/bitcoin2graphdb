a152ecf15ad3cbb8dea1b1ac600558d29ec82087
azuchi <azuchi@haw.co.jp>
2016-03-09 23:12:52 +0900

    Add oa_transaction_spec skeleton

---
+++ spec/graphdb/model/extensions/open_assets/oa_transaction_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Graphdb::Model::Extensions::OpenAssets::OaTransaction' do
+
+  describe 'create oa transaction' do
+    context 'not oa transaction' do
+      
+    end
+
+    context 'issuance transaction' do
+
+    end
+
+    context 'send transaction' do
+
+    end
+  end
+
+end
