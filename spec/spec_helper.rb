219ba99f33b034927eac57b285f1a00cd9c0708a
Jun Okumura <okumura@haw.co.jp>
2019-03-28 17:50:43 +0900

    Supports bolt. Fix timeout option for HTTP. Little fix in spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'base'
 require 'bitcoin_mock'
 require 'neo4j/core/cypher_session/adaptors/http'
+require 'neo4j/core/cypher_session/adaptors/bolt'
 
 RSpec.configure do |config|
 
@@ -53,4 +54,44 @@ end
 def neo4j_session
   neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://localhost:7475', { :basic_auth => { username: 'neo4j', password: 'neo4j' }, :initialize => { :request => {:timeout => 600, :open_timeout => 2}}})
   Neo4j::ActiveBase.on_establish_session { Neo4j::Core::CypherSession.new(neo4j_adaptor) }
+end
+
+def change_neo4j_adaptor(config = nil)
+  if config
+    uri = URI(config[:neo4j][:server])
+    case uri.scheme
+    when 'http'
+      faraday_configurator = proc do |faraday|
+        require 'typhoeus'
+        require 'typhoeus/adapters/faraday'
+        faraday.adapter :typhoeus
+        faraday.options.merge!(config[:neo4j][:initialize][:request])
+      end
+      neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://localhost:7475', faraday_configurator: faraday_configurator)
+    when 'bolt'
+      neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::Bolt.new('bolt://localhost:7473', config[:neo4j][:options])
+    else
+      fail ArgumentError, "Invalid URL: #{uri.inspect}"
+    end
+    graphdb_configure(config)
+  else
+    neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://localhost:7475', { :basic_auth => { username: 'neo4j', password: 'neo4j' }, :initialize => { :request => {:timeout => 600, :open_timeout => 2}}})
+  end
+  Neo4j::ActiveBase.current_adaptor = neo4j_adaptor
+end
+
+def graphdb_configure(config)
+  uri = URI(config[:neo4j][:server])
+  case uri.scheme
+  when 'http'
+    Graphdb.configure do |conf|
+      conf.neo4j_server = 'http://localhost:7475'
+      conf.extensions = config[:extensions]
+    end
+  when 'bolt'
+    Graphdb.configure do |conf|
+      conf.neo4j_server = 'bolt://localhost:7473'
+      conf.extensions = config[:extensions]
+    end
+  end
 end
